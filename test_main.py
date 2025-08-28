148d2672a50439ab7cab70a99db0b20de5ff285a
artalll <153456021+artalll@users.noreply.github.com>
2025-08-28 21:43:41 +0300

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,9 @@
+import unittest
+from main import add
+
+class TestAdd(unittest.TestCase):
+    def test_sum(self):
+        self.assertSetEqual(add(2,3),5)
+
+if __name__=="main":
+    unittest.main()
