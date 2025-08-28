0f9cff6693199d493fd7c326a46dc6757ba01257
artalll <153456021+artalll@users.noreply.github.com>
2025-08-28 21:48:13 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -3,7 +3,7 @@ from main import add
 
 class TestAdd(unittest.TestCase):
     def test_sum(self):
-        self.assertSetEqual(add(2,3),5)
+        self.assertEqual(add(2,3),5)
 
 if __name__=="main":
     unittest.main()
